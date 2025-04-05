0167d2aa3b19c83656a2063f2145b668e5243c25
nawid87 <nawidhussain87@gmail.com>
2025-04-06 01:02:22 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-const socket = io("https://your-backend-url.onrender.com");
+const socket = io("https://omegle-clone-9vbl.onrender.com/");
 
 const localVideo = document.getElementById("localVideo");
 const remoteVideo = document.getElementById("remoteVideo");
